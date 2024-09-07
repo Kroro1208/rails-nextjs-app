e09225826a6d6c7b7c27eff1d85bb0f11002f3f0
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-09-07 19:09:53 +0900

    ログインユーザー取得アクションのリクエストスペック実装

--- rails/spec/requests/api/v1/current/users_spec.rb
+++ rails/spec/requests/api/v1/current/users_spec.rb
@@ -3,7 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Current::Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET api/v1/current/user' do
+    subject { get(api_v1_current_user_path, headers:) } # GETリクエスト定義
+    let(:current_user) { create(:user) }
+    let(:headers) { current_user.create_new_auth_token } # current_userのログインに必要なトークン情報を生成し、headersへの格納を定義
+
+    context 'ヘッダー情報が正常に送られた場合' do
+      it '正常にレコードが取得できる' do
+        subject
+        res = JSON.parse(response.body)
+        expect(res.keys).to eq['id', 'name', 'email']
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'ヘッダー情報が空のままリクエストが送信された場合' do
+      let(:headers) { nill }
+      it 'unauthorrizedを返す' do
+        subject
+        res = JSON.parse(response.body)
+        expect(res['errors']).to eq['ログインもしくはアカウントを登録してください']
+        expect(response).to have_http_status(:unauthorrized)
+      end
+    end
   end
 end
