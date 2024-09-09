bc80ff903770525efe0fc35ff55437a6d73e8cc7
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-09-10 07:53:29 +0900

    さらに修正

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import Link from "next/link";
 import useSWR from "swr";
+
+import Link from "next/link";
 
 import ArticleCard from "./components/ArticleCard";
 import Loading from "./Loading";
