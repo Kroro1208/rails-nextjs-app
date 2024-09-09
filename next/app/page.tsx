8138f247a5a29092a803d79869bb0c29d261a740
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-09-10 07:41:57 +0900

    スタイル修正

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
         {articles.map((article: ArticleProps) => (
           <Link
             key={article.id}
